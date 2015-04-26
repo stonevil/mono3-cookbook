a2942569f08b93d55564d5735847aee0d9389573
Myroslav Rys <stonevil@gmail.com>
2015-04-27 01:05:47 +0300

    Make support compatible with new Supermarket rules

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'stonevil@gmail.com'
 license 'Apache 2.0'
 description 'Installs and configures Mono Framework v3'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
+version '1.0.2'
 
 depends 'git'
 depends 'build-essential'
@@ -13,9 +13,13 @@ depends 'yum'
 depends 'yum-epel'
 depends 'apt'
 
-supports "ubuntu", ">= 12.04", "<= 15.04"
-supports "debian", ">= 7.0", "<= 7.8"
-supports "centos", "<= 7.0"
+supports "ubuntu", "= 12.04"
+supports "ubuntu", "= 12.10"
+supports "ubuntu", "= 14.04"
+supports "ubuntu", "= 14.10"
+supports "ubuntu", "= 15.04"
+supports "debian", "= 7.8"
+supports "centos", "= 7.0"
 supports "redhat"
 supports "fedora"
 supports "amazon"
